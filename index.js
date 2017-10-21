9b4724e985d23163a856c9df938e12b32ca1e689
Lindsey Johnson <redhook@hotmail.com>
2017-10-21 15:43:11 -0600

    created an array of phrases then texted them to make sure I could call them at random

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 $(function(){
-
-	constructor(clues){
-		this.clueArray = clues;
-		this.clue = clues [math.random() * clueArray.length];
-		
+class Game {
+	constructor(){
 		this.puzzles = [
-		"Batman And Robin", 
-		"The Great Gatsby",
-		"Naggers",
-		];
+			"Batman And Robin", 
+			"The Great Gatsby",
+			"Naggers",
+			];
+		this.phrase = this.puzzles[1];
+		this.correctGuess = [];
+		console.log(this.puzzles);
+		console.log(this.phrase);
+
+	}//ends constructor
+	guessLetter(letter){
+		if (this.phrase.includes(letter)){
+			this.correctGuess.push(letter);
+
+		}
+	}//ends guess letter
+	checkWin(){
+		if(this.phrase.length === correctGuess){
+			alert('you won!');
+		}
 	}
+}//ends games
+let game = new Game();
+
 });//ends on load function//
