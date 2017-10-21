f416f539fd51b1c1e4b0dd8a86b78cd02e09798d
Lindsey Johnson <redhook@hotmail.com>
2017-10-21 16:43:24 -0600

    testing

--- index.js
+++ index.js
@@ -6,24 +6,50 @@ class Game {
 			"The Great Gatsby",
 			"Naggers",
 			];
-		this.phrase = this.puzzles[1];
-		this.correctGuess = [];
+		this.phrase = this.puzzles[Math.floor(Math.random() * this.puzzles.length)].split('');
+		this.correctLetters = [];
+		this.phraseLetters =  this.phrase.filter(function(elem, index, self) 
+		{
+			return index == self.indexOf(elem);
+		})
 		console.log(this.puzzles);
 		console.log(this.phrase);
-
 	}//ends constructor
-	guessLetter(letter){
-		if (this.phrase.includes(letter)){
-			this.correctGuess.push(letter);
+	  checkWin(){
+    if (this.correctLetters.length == this.phraseLetters.length) {
+      alert('You win!');
+    }
+    else {
+      this.guessLetter();
+    }
+  } // End of checkIfWon method
 
-		}
-	}//ends guess letter
-	checkWin(){
-		if(this.phrase.length === correctGuess){
-			alert('you won!');
-		}
-	}
-}//ends games
+  displayClue(){
+
+  }
+
+  guessLetter(){
+    let letter = prompt('Guess a letter!');
+
+
+    if(this.phrase.includes(letter)){
+      if(this.correctLetters.includes(letter)){
+        alert("You've already guessed that letter!");
+        this.guessLetter();
+      }
+      else{
+      this.correctLetters.push(letter);
+      }
+    }
+
+    this.checkWin();
+
+  }
+
+
+
+
+}
 let game = new Game();
-
+game.checkWin();
 });//ends on load function//
