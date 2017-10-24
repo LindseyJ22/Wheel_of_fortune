04a68eae294ea85b13c12bf445f4e9330260b6e6
Lindsey Johnson <redhook@hotmail.com>
2017-10-24 15:09:10 -0600

    created an alert window in my html then styled it in css and set it to display none so that I can hopefully call it in my js and display it when the user wins

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ class Game {
 
 	checkWin(){
     	if (this.correctLetters.length == this.splitPhrase.length) {
-      		alert('You win!');
+      		
       		this.startGame();
         }
     } // End of checkWin method
